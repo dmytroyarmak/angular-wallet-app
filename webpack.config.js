114b04f3933452af2f6981a4d3ff62e75f816a5e
Dmytro Yarmak <dmytroyarmak@gmail.com>
2017-02-24 09:18:38 +0200

    Step 6: Configure TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,25 @@ var path = require('path');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: path.join(__dirname, 'src', 'app', 'main.js'),
+  entry: path.join(__dirname, 'src', 'app', 'main.ts'),
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'dist')
   },
   devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         enforce: 'pre',
         loader: 'source-map-loader'
+      },
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader'
       }
     ]
   },
@@ -23,7 +30,7 @@ module.exports = {
     new CopyWebpackPlugin([{
       from: './src'
     }], {
-      ignore: ['*.js']
+      ignore: ['*.ts']
     })
   ]
 };
